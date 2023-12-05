aece292f16215ab3b06bf4ce610902397c472952
JCLEE-ADX <jclee@adxcorp.kr>
2023-12-06 05:46:46 +0900

    [수정] 스토어 키트 아이템 유효성 검증 기능 추가

--- SKANStoreKitTest/ViewController.m
+++ SKANStoreKitTest/ViewController.m
@@ -57,18 +57,37 @@
     };
     
     __weak typeof(self) weakSelf = self;
-    [storeKitVC loadProductWithParameters:dict completionBlock:^(BOOL result, NSError * _Nullable error) {
+    
+    [self lookUpStoreItemById:itemID completion:^(NSData * data, NSError * error) {
+        // itemID 유효성 검증
+        // 참조: https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/LookupExamples.html
+        if(error) { return; }
+        NSError *jsonError = nil;
+        NSDictionary *resDict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
+        if(resDict[@"resultCount"] == nil) { return; }
+        if([resDict[@"resultCount"] integerValue] <= 0) { return; }
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            [storeKitVC loadProductWithParameters:dict completionBlock:^(BOOL result, NSError * _Nullable error) {
+                __strong typeof(self) strongSelf = weakSelf;
+                if(!strongSelf) { return; }
+                if(error) {
+                    [strongSelf appendToMyTextView:[NSString stringWithFormat:@"loadProductWithParameters error: %@", error]];
+                    return;
+                }
+                [strongSelf appendToMyTextView: result ? @"YES" : @"NO"];
+            }];
+        });
+        
         __strong typeof(self) strongSelf = weakSelf;
-        [strongSelf appendToMyTextView:@"completionBlock"];
-        if(error) {
-            [strongSelf appendToMyTextView:[NSString stringWithFormat:@"loadProductWithParameters error: %@", error]];
-            return;
-        }
-        [strongSelf appendToMyTextView: result ? @"YES" : @"NO"];
+        if(!strongSelf) { return; }
+        [strongSelf appendToMyTextView:[NSString stringWithFormat: @"present storeKitVC (%@)", itemID]];
+        /// iOS 특정 버전에서는 스토어 키트를 present 를 해야지만, loadProductWithParameters의 completionBlock 호출 됨.
+        /// 버전 호환성을 위해서 loadProductWithParameters 호출 후, 이어서 바로 presentViewController 호출.
+        [strongSelf presentViewController:storeKitVC animated:YES completion:nil];
     }];
-    [self appendToMyTextView:[NSString stringWithFormat: @"present storeKitVC (%@)", itemID]];
-    [self presentViewController:storeKitVC animated:YES completion:nil];
 }
+
 
 - (void)productViewControllerDidFinish:(SKStoreProductViewController *)viewController {
     [self appendToMyTextView:@"productViewControllerDidFinish"];
@@ -105,6 +124,26 @@
         // Fallback on earlier versions
         return;
     }
+}
+
+- (void)lookUpStoreItemById:(NSString *)itemIdentifier completion:(void(^)(NSData *, NSError *))completionHandler {
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://itunes.apple.com/lookup?id=%@", itemIdentifier]];
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    sessionConfig.timeoutIntervalForRequest = 3.0;
+    sessionConfig.timeoutIntervalForResource = 3.0;
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setCachePolicy:NSURLRequestReloadIgnoringCacheData]; // No-Cache
+    [request setTimeoutInterval:3.0];
+    [[session dataTaskWithRequest:request
+                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (completionHandler) {
+            // 메인 쓰레드에서 실행
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completionHandler(data, error);
+            });
+        }
+    }] resume];
 }
 
 @end
